58a5c67cad270f54f71c6acef5ac14bf5924ce94
Autio Antero Juhani <antero.j.autio@student.jyu.fi>
2025-11-10 17:33:35 +0200

    8

--- osa2/project-reader/src/project.py
+++ osa2/project-reader/src/project.py
@@ -1,17 +1,21 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, license, dev_dependencies, authors, dependencies):
         self.name = name
         self.description = description
-        self.dependencies = dependencies
+        self.license = license
         self.dev_dependencies = dev_dependencies
+        self.authors = authors
+        self.dependencies = dependencies
 
     def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+        return "\n- ".join(dependencies) if len(dependencies) > 0 else "-"
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nLicense: {self.license}"
+            f"\n\nAuthors: \n- {self._stringify_dependencies(self.authors)}"
+            f"\n\nDependencies: \n- {self._stringify_dependencies(self.dependencies)}"
+            f"\n\nDevelopment dependencies: \n- {self._stringify_dependencies(self.dev_dependencies)}"
         )
--- osa2/project-reader/src/project_reader.py
+++ osa2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import tomli
 
 
 class ProjectReader:
@@ -9,7 +10,11 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
-
+        data = tomli.loads(content)
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(data["tool"]["poetry"]["name"], 
+        data["tool"]["poetry"]["description"], 
+        data["tool"]["poetry"]["license"], 
+        data["tool"]["poetry"]["group"]["dev"]["dependencies"],
+        data["tool"]["poetry"]["authors"],
+        data["tool"]["poetry"]["dependencies"])
